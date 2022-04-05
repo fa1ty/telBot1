402334d245d87ca7f57b3ab4fbe59b322022ee0f
fa1ly <mdudnik1998@gmail.com>
2022-04-05 17:04:22 +0300

    v.001

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import telebot
+from telebot import types
+import requests
+import bs4
 
 bot = telebot.TeleBot('5151693175:AAGacM5Sv9XLJDrlgpaFoKDLBff5UUq6KMs')
 
@@ -19,6 +22,73 @@ def start(message, res=False):
 def get_text_messages(message):
     chat_id = message.chat.id
     ms_text = message.text
-    bot.send_message(chat_id, text="Я тебя слышу!!! Ваше сообщение: " + ms_text)
 
-bot.polling(none_stop=True, interval=0)
+    if ms_text == "Главное меню" or ms_text == "👋 Главное меню" or ms_text == "Вернуться в главное меню":  # ..........
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton("Развлечения")
+        btn2 = types.KeyboardButton("WEB-камера")
+        btn3 = types.KeyboardButton("Управление")
+        back = types.KeyboardButton("Помощь")
+        markup.add(btn1, btn2, btn3, back)
+        bot.send_message(chat_id, text="Вы в главном меню", reply_markup=markup)
+
+    elif ms_text == "Развлечения":  # ..................................................................................
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton("Прислать собаку")
+        btn2 = types.KeyboardButton("Прислать анекдот")
+        back = types.KeyboardButton("Вернуться в главное меню")
+        markup.add(btn1, btn2, back)
+        bot.send_message(chat_id, text="Развлечения", reply_markup=markup)
+
+    elif ms_text == "/dog" or ms_text == "Прислать собаку":  # .........................................................
+        bot.send_photo(chat_id, photo=get_dogURL(), caption="Вот тебе собачка!")
+
+    elif ms_text == "Прислать анекдот":  # .............................................................................
+        bot.send_message(chat_id, text=get_anekdot())
+
+    elif ms_text == "WEB-камера":
+        bot.send_message(chat_id, text="еще не готово...")
+
+    elif ms_text == "Управление":  # ...................................................................................
+        bot.send_message(chat_id, text="еще не готово...")
+
+    elif ms_text == "Помощь" or ms_text == "/help":  # .................................................................
+        bot.send_message(chat_id, "Автор: Я")
+        key1 = types.InlineKeyboardMarkup()
+        btn1 = types.InlineKeyboardButton(text="Напишите автору", url="https://t.me/ChiKatia")
+        key1.add(btn1)
+        img = open('AVA.jpg', 'rb')
+        bot.send_photo(message.chat.id, img, reply_markup=key1)
+
+    else:  # ...........................................................................................................
+        bot.send_message(chat_id, text="Я тебя слышу!!! Ваше сообщение: " + ms_text)
+
+    # -----------------------------------------------------------------------
+    def get_anekdot():
+        array_anekdots = []
+        req_anek = requests.get('http://anekdotme.ru/random')
+        if req_anek.status_code == 200:
+            soup = bs4.BeautifulSoup(req_anek.text, "html.parser")
+            result_find = soup.select('.anekdot_text')
+            for result in result_find:
+                array_anekdots.append(result.getText().strip())
+        if len(array_anekdots) > 0:
+            return array_anekdots[0]
+        else:
+            return ""
+
+    # -----------------------------------------------------------------------
+    def get_dogURL():
+        url = ""
+        req = requests.get('https://random.dog/woof.json')
+        if req.status_code == 200:
+            r_json = req.json()
+            url = r_json['url']
+            # url.split("/")[-1]
+        return url
+
+    # -----------------------------------------------------------------------
+
+    bot.polling(none_stop=True, interval=0)  # Запускаем бота
+
+    print()
