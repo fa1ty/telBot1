c45e4216452a3997b54ec64f509c44d317e1b0c8
fa1ly <mdudnik1998@gmail.com>
2022-04-05 16:38:13 +0300

    api bot

--- main.py
+++ main.py
@@ -2,3 +2,15 @@ import telebot
 
 bot = telebot.TeleBot('5151693175:AAGacM5Sv9XLJDrlgpaFoKDLBff5UUq6KMs')
 
+@bot.message_handler(commands=["start"])
+def start(message, res=False):
+    chat_id = message.chat.id
+
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton("👋 Главное меню")
+    btn2 = types.KeyboardButton("❓ Помощь")
+    markup.add(btn1, btn2)
+
+    bot.send_message(chat_id,
+                     text="Привет, {0.first_name}! Я тестовый бот для курса программирования на языке ПаЙтон".format(
+                         message.from_user), reply_markup=markup)
